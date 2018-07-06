ee48bb1e6ab62729c77fab239b6c523f39089418
gzhawk <gzhawk@hotmail.com>
2018-07-06 11:27:43 +0800

    minor change on flash program demo

--- sys_main/UI/ui_flash_prog.h
+++ sys_main/UI/ui_flash_prog.h
@@ -9,18 +9,17 @@
 #define FLASH_INFO_LEN     36
 
 #define FLASH_F_BLOB       ROOT_PATH"flash_prog\\unified.blob"
-#define FLASH_F_RES        ROOT_PATH"flash_prog\\ASTC4x4.bin"
-
-#define FLASH_ADDR_BLOB    0
-#define FLASH_ADDR_RES     4096
+//#define FLASH_F_RES        ROOT_PATH"flash_prog\\ASTC4x4.bin"
+#define FLASH_F_RES        ROOT_PATH"flash_prog\\ASTC4x4.raw"
+//#define FLASH_F_RES        ROOT_PATH"flash_prog\\512x512.anim"
 
 typedef enum PROG_STEP_ {
     P_STAT = 0,
     P_STAT_F,
-    P_CHECK_BLOB,
-    P_CHECK_BLOB_F,
-    P_CHECK_FILE,
-    P_CHECK_FILE_F,
+    P_CK_BLOB,
+    P_CK_BLOB_F,
+    P_CK_FILE,
+    P_CK_FILE_F,
     P_ERASE,
     P_PG_BLOB,
     P_PG_BLOB_F,
@@ -37,20 +36,20 @@ typedef enum PROG_STEP_ {
 
 FTC8 prog_info[P_END][FLASH_INFO_LEN] = {
 "Set Flash to full",
-"Fail to set Flash to full",
+"Set Flash to full fail",
 "Check blob file",
 "Check blob file fail",
-"Check file",
-"Check file fail",
+"Check user file",
+"Check user file fail",
 "Erase Flash",
-"Write blob file to Flash",
-"Fail to write blob file",
+"Write blob file",
+"Write blob file fail",
 "Verify blob file",
-"Fail to verify blob file",
-"Write file to Flash",
-"Fail to write file",
-"Verify file",
-"Fail to verify file",
+"Verify blob file fail",
+"Write user file",
+"Write user file fail",
+"Verify user file",
+"Verify user file fail",
 "Success",
 "Unknown error",
 };
@@ -64,19 +63,20 @@ FTVOID flash_ui (FTU32 para)
 	if (appGP.appPara == P_SUCC) {
 	    HAL_CmdBufIn(COLOR_RGB(0, 255, 0));
 	} else if (appGP.appPara == P_STAT_F || 
+		appGP.appPara == P_CK_BLOB_F ||
+		appGP.appPara == P_CK_FILE_F ||
 		appGP.appPara == P_PG_BLOB_F ||
 		appGP.appPara == P_VR_BLOB_F ||
 		appGP.appPara == P_PG_RES_F ||
 		appGP.appPara == P_VR_RES_F ||
-		appGP.appPara == P_CHECK_BLOB_F ||
-		appGP.appPara == P_CHECK_FILE_F ||
         appGP.appPara == P_ERROR ) {
 	    HAL_CmdBufIn(COLOR_RGB(255, 0, 0));
 	} else {
 	    HAL_CmdBufIn(COLOR_RGB(255, 255, 255));
 	}
-	CoCmd_TEXT(EVE_LCD_WIDTH / 2, EVE_LCD_HIGH / 2, 24, OPT_CENTER , &prog_info[appGP.appPara][0]);
-    
+
+	CoCmd_TEXT(EVE_LCD_WIDTH / 2, EVE_LCD_HIGH / 2, 24, OPT_CENTER , (FTC8 *)&prog_info[appGP.appPara][0]);
+
     HAL_CmdBufIn(END());
     HAL_CmdBufIn(DISPLAY());
 	HAL_CmdBufIn(CMD_SWAP);
@@ -88,8 +88,8 @@ FTVOID flash_ui (FTU32 para)
 		appGP.appPara == P_VR_BLOB_F ||
 		appGP.appPara == P_PG_RES_F ||
 		appGP.appPara == P_VR_RES_F ||
-		appGP.appPara == P_CHECK_BLOB_F ||
-		appGP.appPara == P_CHECK_FILE_F ||
+		appGP.appPara == P_CK_BLOB_F ||
+		appGP.appPara == P_CK_FILE_F ||
 		appGP.appPara == P_SUCC ||
         appGP.appPara == P_ERROR ) {
 	    appGP.appIndex = 0;
@@ -110,55 +110,59 @@ FTVOID flash_ui (FTU32 para)
 */
 FTVOID flash_prog (FTU32 para)
 {
+    static FTU32 f_addr = 0;
+
     switch (appGP.appPara) {
         case P_STAT:
             if (!appFlashSetFull()) {
-                appGP.appPara = P_CHECK_BLOB;
+                appGP.appPara = P_CK_BLOB;
             } else {
                 appGP.appPara = P_STAT_F;
             }
             break;
-        case P_CHECK_BLOB:
+        case P_CK_BLOB:
             if(appFileExist((FTC8 *)FLASH_F_BLOB)) {
-                appGP.appPara = P_CHECK_FILE;
+                appGP.appPara = P_CK_FILE;
             } else {
-                appGP.appPara = P_CHECK_BLOB_F;
+                appGP.appPara = P_CK_BLOB_F;
             }
             break;
-        case P_CHECK_FILE:
+        case P_CK_FILE:
             if(appFileExist((FTC8 *)FLASH_F_RES)) {
                 appGP.appPara = P_ERASE;
             } else {
-                appGP.appPara = P_CHECK_FILE_F;
+                appGP.appPara = P_CK_FILE_F;
             }
             break;
         case P_ERASE:
             appFlashErase();
             appGP.appPara = P_PG_BLOB;
+			f_addr = 0;
             break;
         case P_PG_BLOB:
-            if (appFlashProg((FTU8 *)FLASH_F_BLOB, FLASH_ADDR_BLOB)) {
+            f_addr = appFlashProg((FTU8 *)FLASH_F_BLOB, 0);
+            if (f_addr) {
                 appGP.appPara = P_VR_BLOB;
             } else {
 				appGP.appPara = P_PG_BLOB_F;
             }
             break;
         case P_VR_BLOB:
-            if (appFlashVerify((FTU8 *)FLASH_F_BLOB, FLASH_ADDR_BLOB)) {
+            if (appFlashVerify((FTU8 *)FLASH_F_BLOB, 0)) {
                 appGP.appPara = P_PG_RES;
             } else {
-				appGP.appPara = P_PG_BLOB_F;
+				appGP.appPara = P_VR_BLOB_F;
             }
             break;
         case P_PG_RES:
-            if (appFlashProg((FTU8 *)FLASH_F_RES, FLASH_ADDR_RES)) {
+            if (appFlashProg((FTU8 *)FLASH_F_RES, f_addr)) {
                 appGP.appPara = P_VR_RES;
             } else {
 				appGP.appPara = P_PG_RES_F;
             }
             break;
         case P_VR_RES:
-            if (appFlashVerify((FTU8 *)FLASH_F_RES, FLASH_ADDR_RES)) {
+            if (appFlashVerify((FTU8 *)FLASH_F_RES, f_addr)) {
                 appGP.appPara = P_SUCC;
             } else {
 				appGP.appPara = P_VR_RES_F;
