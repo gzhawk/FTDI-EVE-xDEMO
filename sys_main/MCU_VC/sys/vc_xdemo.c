5ace099d48eef3f38c0fc9bf0f560eca7ab52f27
gzhawk <gzhawk@hotmail.com>
2018-07-18 15:58:36 +0800

    add a VC platform
    mix vc2010, vc2012, vc2017 together

---
+++ sys_main/MCU_VC/sys/vc_xdemo.c
@@ -0,0 +1,463 @@
+#include "platform.h"
+
+#if defined(VC_EMULATOR)
+BT8XXEMU_Emulator *gEmulator = NULL;
+#endif
+
+FTVOID rdStart ( FTU32 addr )
+{
+#if defined(VC_MPSSE)
+    FTU8 tmp[SPI_RXCMD_LEN] = {0};
+    FTU32 send;
+
+    tmp[0] = (FTU8)(addr >> 16);
+    tmp[1] = (FTU8)(addr >> 8);
+    tmp[2] = (FTU8)addr;
+    tmp[3] = 0; /* dummy required by MPSSE */
+
+    SPI_Write(ftHandle,tmp,SPI_RXCMD_LEN,&send,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_ENABLE);
+#elif defined(VC_EMULATOR)
+    FT8XXEMU_cs(1);
+    FT8XXEMU_transfer((FTU8)(addr >> 16));
+    FT8XXEMU_transfer((FTU8)(addr >> 8));
+    FT8XXEMU_transfer((FTU8)addr);
+    FT8XXEMU_transfer(0);
+#else
+#endif
+}
+
+FTVOID wrStart ( FTU32 addr )
+{
+#if defined(VC_MPSSE)
+    FTU8 tmp[SPI_TXCMD_LEN] = {0};
+    FTU32 send;
+
+    tmp[0] = (FTU8)(0x80 | (addr >> 16));
+    tmp[1] = (FTU8)(addr >> 8);
+    tmp[2] = (FTU8)addr;
+
+    SPI_Write(ftHandle,tmp,SPI_TXCMD_LEN,&send,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_ENABLE);
+#elif defined(VC_EMULATOR)
+    FT8XXEMU_cs(1);
+    FT8XXEMU_transfer((FTU8)(addr >> 16) | 0x80);
+    FT8XXEMU_transfer((FTU8)(addr >> 8));
+    FT8XXEMU_transfer((FTU8)addr);
+#else
+#endif
+}
+
+FTVOID HAL_Cfg ( FTU8 cfg )
+{
+#if defined(VC_MPSSE)
+    FTU32 send;
+    FTU8 tmp[SPI_TXCMD_LEN] = {0};
+
+    tmp[0] = cfg;
+
+    SPI_Write(ftHandle,tmp,SPI_TXCMD_LEN,&send, 
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_ENABLE | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_DISABLE);
+#elif defined(VC_EMULATOR)
+    FT8XXEMU_transfer(cfg);
+#else
+#endif
+}
+
+FTVOID HAL_Write8 ( FTU32 addr, FTU8 data )
+{
+#if defined(VC_MPSSE)
+    FTU8 tmp[SPI_TXCMD_LEN+1] = {0};
+    FTU32 send;
+
+    tmp[0] = (FTU8)(0x80 | (addr >> 16));
+    tmp[1] = (FTU8)(addr >> 8);
+    tmp[2] = (FTU8)addr;
+    tmp[3] = data;
+
+    SPI_Write(ftHandle,tmp,SPI_TXCMD_LEN+1,&send,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_ENABLE | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_DISABLE);
+#elif defined(VC_EMULATOR)
+    wrStart(addr);
+    FT8XXEMU_transfer(data);
+    FT8XXEMU_cs(0);
+#else
+#endif
+}
+
+FTVOID HAL_Write8Src ( FTU32 addr, FTU8 *src, FTU32 len )
+{
+#if defined(VC_MPSSE)
+    FTU32 i;
+
+    wrStart(addr);
+    SPI_Write(ftHandle,src,len,&i,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_DISABLE);
+#elif defined(VC_EMULATOR)
+    FTU32 i;
+
+    wrStart(addr);
+    for (i = 0; i < len; i++) {
+        FT8XXEMU_transfer(src[i]);
+    }
+    FT8XXEMU_cs(0);
+#else
+#endif
+}
+
+FTVOID HAL_Write16 ( FTU32 addr, FTU16 data )
+{
+#if defined(VC_MPSSE)
+    FTU8 tmp[SPI_TXCMD_LEN+2] = {0};
+    FTU32 send;
+
+    tmp[0] = (FTU8)(0x80 | (addr >> 16));
+    tmp[1] = (FTU8)(addr >> 8);
+    tmp[2] = (FTU8)addr;
+    tmp[3] = (FTU8)(data & 0xff);
+    tmp[4] = (FTU8)(data >> 8);
+
+    SPI_Write(ftHandle,tmp,SPI_TXCMD_LEN+2,&send,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_ENABLE | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_DISABLE);
+#elif defined(VC_EMULATOR)
+    wrStart(addr);
+
+    FT8XXEMU_transfer((FTU8)data&0xFF);
+    FT8XXEMU_transfer((FTU8)(data>>8)&0xFF);
+
+    FT8XXEMU_cs(0);
+#else
+#endif
+}
+
+FTVOID HAL_Write32 ( FTU32 addr, FTU32 data )
+{
+#if defined(VC_MPSSE)
+    FTU8 tmp[SPI_TXCMD_LEN+4] = {0};
+    FTU32 send;
+
+    tmp[0] = (FTU8)(0x80 | (addr >> 16));
+    tmp[1] = (FTU8)(addr >> 8);
+    tmp[2] = (FTU8)addr;
+    tmp[3] = (FTU8)data & 0xff;
+    tmp[4] = (FTU8)(data >> 8);
+    tmp[5] = (FTU8)(data >> 16);
+    tmp[6] = (FTU8)(data >> 24);
+
+    SPI_Write(ftHandle,tmp,SPI_TXCMD_LEN+4,&send,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_ENABLE | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_DISABLE);
+#elif defined(VC_EMULATOR)
+    wrStart(addr);
+
+    FT8XXEMU_transfer((FTU8)data&0xFF);
+    FT8XXEMU_transfer((FTU8)(data>>8)&0xFF);
+    FT8XXEMU_transfer((FTU8)(data>>16)&0xFF);
+    FT8XXEMU_transfer((FTU8)(data>>24)&0xFF);
+
+    FT8XXEMU_cs(0);
+#else
+#endif
+}
+
+FTU8 HAL_Read8 ( FTU32 addr )
+{
+#if defined(VC_MPSSE)
+    FTU8 tmp[SPI_RXCMD_LEN] = {0};
+    FTU32 recv;
+
+    rdStart(addr);
+
+    SPI_Read(ftHandle,tmp,1,&recv,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_DISABLE);
+
+    return tmp[0];
+#elif defined(VC_EMULATOR)
+    FTU8 tmp;
+
+    rdStart(addr);
+
+    tmp = FT8XXEMU_transfer(0);
+
+    FT8XXEMU_cs(0);
+
+    return tmp;
+#else
+#endif
+}
+
+FTU32 HAL_Read8Buff ( FTU32 addr, FTU8 *buff, FTU32 len )
+{
+#if defined(VC_MPSSE)
+    FTU32 recv;
+
+    rdStart(addr);
+
+    SPI_Read(ftHandle,buff,len,&recv,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_DISABLE);
+
+    return recv;
+#elif defined(VC_EMULATOR)
+    FTU32 tmp = len;
+    FTU8 *p = buff;
+
+    rdStart(addr);
+
+    while (tmp--) {
+        *p = FT8XXEMU_transfer(0);
+        p++;
+    }
+
+    FT8XXEMU_cs(0);
+
+    return len;
+#else
+#endif
+}
+
+FTU16 HAL_Read16 ( FTU32 addr )
+{
+#if defined(VC_MPSSE)
+    FTU8 tmp[SPI_RXCMD_LEN] = {0};
+    FTU32 recv;
+
+    rdStart(addr);
+
+    SPI_Read(ftHandle,tmp,2,&recv,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_DISABLE);
+
+    return (tmp[0]|(tmp[1]<<8));
+#elif defined(VC_EMULATOR)
+    FTU16 tmp = 0;
+
+    rdStart(addr);
+
+    tmp = (FTU16)FT8XXEMU_transfer(0);
+    tmp |= (FTU16)FT8XXEMU_transfer(0) << 8;
+
+    FT8XXEMU_cs(0);
+
+    return tmp;
+#else
+#endif
+}
+
+FTU32 HAL_Read32 ( FTU32 addr )
+{
+#if defined(VC_MPSSE)
+    FTU8 tmp[SPI_RXCMD_LEN+4] = {0};
+    FTU32 recv;
+
+    rdStart(addr);
+
+    SPI_Read(ftHandle,tmp,4,&recv,
+            SPI_TRANSFER_OPTIONS_SIZE_IN_BYTES | 
+            SPI_TRANSFER_OPTIONS_CHIPSELECT_DISABLE);
+
+    return (tmp[0]|(tmp[1]<<8)|(tmp[2]<<16)|(tmp[3]<<24));
+#elif defined(VC_EMULATOR)
+    FTU32 tmp = 0;
+
+    rdStart(addr);
+
+    tmp = (FTU32)FT8XXEMU_transfer(0);
+    tmp |= (FTU32)FT8XXEMU_transfer(0) << 8;
+    tmp |= (FTU32)FT8XXEMU_transfer(0) << 16;
+    tmp |= (FTU32)FT8XXEMU_transfer(0) << 24;
+
+    FT8XXEMU_cs(0);
+
+    return tmp;
+#else
+#endif
+}
+
+FTVOID vc_apps_sys_dummy (FTU32 para)
+{
+    /* do nothing */
+}
+
+FTVOID vc_dumy_print (char *p)
+{
+    /* do nothing */
+}
+
+FTVOID HAL_ili9488 (FTVOID)
+{
+    /* do nothing */
+}
+
+FTVOID HAL_speed_up (FTU32 type)
+{
+    /* do nothing */
+}
+
+FTVOID HAL_PwdCyc ( FTU8 OnOff )
+{
+    /* do nothing */
+}
+
+FTVOID HAL_SpiInit ( FTVOID )
+{
+    /* do nothing */
+}
+
+FTVOID HAL_preparation (FTVOID)
+{
+    /* do nothing */
+}
+
+FTVOID HAL_invaild_tag (FTC8 *dataPath)
+{
+	remove(dataPath);
+}
+
+FTU8 HAL_is_tag_vaild (FTC8 *dataPath)
+{
+	FILE *pF;
+	
+	pF = fopen(dataPath,"rb");
+	if(NULL == pF) {
+		return 0;
+	}
+	fclose(pF);
+	return 1;
+}
+
+FTVOID HAL_save_cdata (FTC8 *dataPath, FTU8 *p)
+{
+	FILE *pF;
+	
+	pF = fopen(dataPath,"wb");
+	if(NULL == pF) {
+		FTPRINT("\nHAL_save_cdata: fail to open file");
+		/* don't stop the bootup, 
+		   if only can not open file */
+		return;
+	}
+
+	fwrite(p, 4, EVE_CAL_PARA_NUM, pF);
+	fclose(pF);
+}
+
+FTVOID HAL_restore_cdata (FTC8 *dataPath, FTU8 *p)
+{
+	FILE *pF;
+	
+	pF = fopen(dataPath,"rb");
+	fread(p, 4, EVE_CAL_PARA_NUM, pF);
+	fclose(pF);
+}
+
+FTVOID HAL_vaild_tag (FTVOID)
+{
+	/* already save it to file, the file is the 'tag' */
+	/* give some time to file system to finish operation */
+	FTDELAY(100);
+}
+
+FTVOID FT8XXEMU_cs(FT8 i)
+{
+	BT8XXEMU_chipSelect(gEmulator, i);
+}
+
+FTU32 FT8XXEMU_transfer(FTU32 data)
+{
+    return BT8XXEMU_transfer(gEmulator, data);
+}
+
+FTU32 HAL_WriteSrcToDes (FTU32 handle, FTU32 src, FTU32 des, FTU32 len)
+{
+    fseek((FILE *)handle, src, SEEK_SET);
+
+    return fread((FTVOID *)des, 1, len, (FILE *)handle);
+}
+
+FTU8 * HAL_LoopMemMalloc (FTU32 handle, FTU32 src, FTU32 len)
+{
+    return (FTU8 *)malloc(len);
+}
+
+FTVOID HAL_LoopMemRead (FTU32 handle, FTU8 **ppbuf, FTU32 len)
+{
+    fread(*ppbuf, 1, len, (FILE *)handle);
+}
+
+FTVOID HAL_LoopMemFree (FTU32 buf)
+{
+    free((FTVOID *)buf);
+}
+
+FTU32 HAL_SegFileOpen (FTU8 *path)
+{
+    FILE *pFile;
+
+    if(NULL == path)
+    {  
+        FTPRINT("\nHAL_SegFileOpen: path null");
+        return 0;
+    }
+
+    pFile = fopen((FTC8 *)path,"rb");
+    if(NULL == pFile)
+    {  
+        FTPRINT("\nHAL_SegFileOpen: file open error");
+        return 0;
+    }
+
+    return (FTU32)pFile;
+}
+
+FTU32 HAL_SegFileSize (FTU32 handle)
+{
+    FTU32 size;
+
+    fseek((FILE*)handle,0,SEEK_END);
+
+    size = ftell((FILE*)handle);
+    
+    fseek((FILE*)handle,0,SEEK_SET);
+
+    return BYTES4ALIGN(size);
+}
+
+FTVOID HAL_SegFileClose (FTU32 handle)
+{
+    fclose((FILE*)handle);
+}
+
+FTU8 HAL_ZlibCompressed (FTU32 handle, FTU32 src)
+{
+    FTU8 header[2] = {0};
+    
+    fread(header, 1, 2, (FILE*)handle);
+    fseek((FILE*)handle, src, SEEK_SET);
+    /*
+       zLib compression header
+       +---+---+
+       |CMF|FLG|
+       +---+---+
+
+       78 01 - No Compression/low
+       78 9C - Default Compression
+       78 DA - Best Compression 
+     */
+    if (header[0] == 0x78 && header[1] == 0x9C) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
---
+++ sys_main/MCU_VC/sys/vc_xdemo.h
@@ -0,0 +1,172 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+#include <time.h>
+
+#if defined(VC_MPSSE)
+#include "libMPSSE_spi.h"
+#endif
+
+#if defined(VC_EMULATOR)
+#include <direct.h>
+#include <io.h>
+#endif
+
+typedef const char     FTC8;
+typedef signed char    FT8;
+typedef unsigned char  FTU8;
+typedef signed short   FT16;
+typedef unsigned short FTU16;
+typedef signed int     FT32;
+typedef unsigned int   FTU32;
+typedef signed long    FT64;
+typedef unsigned long  FTU64;
+#define FTVOID void
+
+#if defined(_MSC_VER) && defined(VC_EMULATOR)
+typedef unsigned __int8 uint8_t;
+typedef signed __int8 int8_t;
+typedef unsigned __int16 uint16_t;
+typedef signed __int16 int16_t;
+typedef unsigned __int32 uint32_t;
+typedef signed __int32 int32_t;
+typedef unsigned __int64 uint64_t;
+typedef signed __int64 int64_t;
+#endif
+
+#define USED_CMD_BUF
+
+#define PROGMEM 
+#define FLASH8 FT8
+#define FLASHU8 FTU8
+#define FLASHRD8(a) (*(FTU8 *)(a))
+
+#define FTDELAY(mS)    Sleep(mS)
+/* actually it's a fake function */
+#define FTUDELAY(mS)    Sleep(mS/1000)
+#define ROOT_PATH "..\\..\\res\\"
+#define CDATA_PATH ROOT_PATH"cdata.bin"
+
+#define STATIC static
+
+#ifdef DBG_PRINT
+#define FTPRINT     printf
+#define DBGPRINT    appUI_DbgPrint(__FUNCTION__,__LINE__)
+#define SYS_INIT    printf("\r\nVer: %s",APPS_VER)
+#else
+#define FTPRINT     vc2012emu_dumy_print
+#define DBGPRINT
+#define SYS_INIT 
+#endif
+
+#define FTRANDOM(M) (rand()%(M))
+
+#define EVE_CAL_PARA_NUM    (6)
+
+#define APPS_SYS vc_apps_sys_dummy
+
+#if defined(VC_MPSSE)
+#define SPI_CLK_12M 12000000
+#define SPI_CLK_15M 15000000
+#define SPI_L_TIMER 2
+#define SPI_INIDIR_IN 0x00000000
+#define SPI_INIDIR_OUT 0x00000001
+#define SPI_INIVAL_LOW 0x00000000
+#define SPI_INIVAL_HIGH 0x00000100
+#define SPI_ENDDIR_IN 0x00000000
+#define SPI_ENDDIR_OUT 0x00010000
+#define SPI_ENDVAL_LOW 0x00000000
+#define SPI_ENDDIR_HIGH 0x01000000
+#define SPI_TXCMD_LEN 3
+#define SPI_RXCMD_LEN 4
+
+
+#define FTMAIN FT32 main (FT32 argc,FT8 *argv[]) 
+#define FTDUMMY
+#define SYS_END    return 0
+
+#endif
+
+#if defined(VC_EMULATOR)
+typedef uint32_t argb8888;
+#include "bt8xxemu_Emulator.h"
+extern BT8XXEMU_Emulator *gEmulator;
+
+#define BTFLASH_DATA_FILE   L"..\\..\\res\\flash\\bt81X.flash"
+#define BTFLASH_DEVICE_TYPE L"mx25lemu"
+#define BTFLASH_SIZE        (16 * 1024 * 1024)
+
+#if defined(DEF_BT81X)
+#define EVEMODE BT8XXEMU_EmulatorBT815
+#elif defined(DEF_81X)
+#define EVEMODE BT8XXEMU_EmulatorFT810
+#else
+#define EVEMODE BT8XXEMU_EmulatorFT800
+#endif
+
+#define FTMAIN FTVOID mcu (BT8XXEMU_Emulator *sender, void *context) 
+#define FTDUMMY \
+                int main(FT32 argc,FT8 *argv[]) \
+                { \
+                  BT8XXEMU_Flash *flash = NULL; \
+                  BT8XXEMU_FlashParameters flashParams; \
+				  BT8XXEMU_EmulatorParameters params; \
+                  \
+                  BT8XXEMU_Flash_defaults(BT8XXEMU_VERSION_API, &flashParams); \
+                  wcscpy(flashParams.DeviceType, BTFLASH_DEVICE_TYPE); \
+                  wcscpy(flashParams.DataFilePath, BTFLASH_DATA_FILE); \
+                  flashParams.SizeBytes = BTFLASH_SIZE; \
+                  flashParams.StdOut = 1; \
+                  flash = BT8XXEMU_Flash_create(BT8XXEMU_VERSION_API, &flashParams); \
+                  \
+				  BT8XXEMU_defaults(BT8XXEMU_VERSION_API, &params, EVEMODE); \
+				  params.Flags |= BT8XXEMU_EmulatorEnableStdOut; \
+				  params.Main = mcu; \
+				  params.Flash = flash; \
+				  BT8XXEMU_run(BT8XXEMU_VERSION_API, &gEmulator, &params); \
+                  BT8XXEMU_stop(gEmulator); \
+                  BT8XXEMU_destroy(gEmulator); \
+                  BT8XXEMU_Flash_destroy(flash); \
+                  \
+				  return 0; \
+			    }
+#define SYS_END BT8XXEMU_stop(gEmulator)
+
+FTVOID FT8XXEMU_cs(FT8 i);
+FTU32 FT8XXEMU_transfer(FTU32 data);
+#endif
+
+FTVOID vc_dumy_print (char *p);
+FTVOID vc_apps_sys_dummy (FTU32 para);
+
+FTVOID HAL_restore_cdata (FTC8 *dataPath, FTU8 *p);
+FTU8 HAL_is_tag_vaild (FTC8 *dataPath);
+FTVOID HAL_vaild_tag (FTVOID);
+FTVOID HAL_invaild_tag (FTC8 *dataPath);
+FTVOID HAL_save_cdata (FTC8 *dataPath, FTU8 *p);
+FTVOID HAL_ili9488 (FTVOID);
+FTVOID HAL_speed_up (FTU32 type);
+FTVOID HAL_PwdCyc ( FTU8 OnOff );
+FTVOID HAL_SpiInit ( FTVOID );
+FTVOID HAL_preparation (FTVOID);
+FTU32 HAL_WriteSrcToDes (FTU32 handle, FTU32 src, FTU32 des, FTU32 len);
+FTU8 * HAL_LoopMemMalloc (FTU32 handle, FTU32 src, FTU32 len);
+FTVOID HAL_LoopMemRead (FTU32 handle, FTU8 **ppbuf, FTU32 len);
+FTVOID HAL_LoopMemFree (FTU32 buf);
+FTU32 HAL_SegFileOpen (FTU8 *path);
+FTU32 HAL_SegFileSize (FTU32 handle);
+FTVOID HAL_SegFileClose (FTU32 handle);
+FTU8 HAL_ZlibCompressed (FTU32 handle, FTU32 src);
+
+
+FTVOID HAL_Cfg ( FTU8 cfg );
+FTVOID HAL_Write8 ( FTU32 addr, FTU8 data );
+FTVOID HAL_Write8Src ( FTU32 addr, FTU8 *src, FTU32 len );
+FTVOID HAL_Write16 ( FTU32 addr, FTU16 data );
+FTVOID HAL_Write32 ( FTU32 addr, FTU32 data );
+FTU8 HAL_Read8 ( FTU32 addr );
+FTU32 HAL_Read8Buff ( FTU32 addr, FTU8 *buff, FTU32 len );
+FTU16 HAL_Read16 ( FTU32 addr );
+FTU32 HAL_Read32 ( FTU32 addr );
+
