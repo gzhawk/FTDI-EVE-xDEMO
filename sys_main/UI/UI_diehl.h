20285aebfd9cee1c014a008ce5bb55a4af051066
gzhawk <gzhawk@hotmail.com>
2018-08-28 16:49:41 +0800

    5.C.47.1
    make it as simple as possible

--- sys_main/UI/UI_diehl.h
+++ sys_main/UI/UI_diehl.h
@@ -11,10 +11,8 @@
 #error "program res/diehl/bt81x.flash to on-board flash, then comment this line"
 #endif
 
-#define ANIM_1_ADDR  14703936
-#define ANIM_1_FRAME 478
-#define ANIM_2_ADDR  15369984
-#define ANIM_2_FRAME 34
+#define ANIM_ADDR  2052928
+#define ANIM_FRAME 480
 
 FTVOID loop_frame(FTU32 *pframe, FTU32 max)
 {
@@ -27,8 +25,8 @@ FTVOID loop_frame(FTU32 *pframe, FTU32 max)
 
 FTVOID flash_anim(FTU32 para)
 {
-	static FTU8 load = 1, set = 0;
-	static FTU32 addr = ANIM_1_ADDR, index = 0, max = ANIM_1_FRAME;
+	static FTU8 load = 1;
+	static FTU32 addr = ANIM_ADDR, index = 0, max = ANIM_FRAME;
 
 	if (load) {
 		if (!appFlashSetFull()) {
@@ -43,8 +41,10 @@ FTVOID flash_anim(FTU32 para)
 	HAL_CmdBufIn(CLEAR_COLOR_RGB(255, 255, 255));
 	HAL_CmdBufIn(CLEAR(1, 1, 1));
 
-    /* display one frame */
-    CoCmd_ANIMFRAME(EVE_LCD_WIDTH/2+5, EVE_LCD_HIGH/2, addr, index);
+    /* display one frame, at this point, 
+       ANIMFRAME would have some shifted
+       instead of perfect centrol of the image*/
+    CoCmd_ANIMFRAME(EVE_LCD_WIDTH/2+8, EVE_LCD_HIGH/2+12, addr, index);
     
     /* change the frame index to make it 'animation' */
     loop_frame(&index, max);
@@ -54,22 +54,6 @@ FTVOID flash_anim(FTU32 para)
 	HAL_CmdBufIn(CMD_SWAP);
 
 	HAL_BufToReg(RAM_CMD, 0);
-
-    while (TOUCHED) {
-        set = 1;
-    };
-    
-    if (set) {
-        if (addr == ANIM_1_ADDR) {
-            addr = ANIM_2_ADDR;
-            max = ANIM_2_FRAME;
-        } else {
-            addr = ANIM_1_ADDR;
-            max = ANIM_1_FRAME;
-        }
-        index = 0;
-        set = 0;
-    }
 
 	appGP.appIndex = 0;
 }
