2bfc1f3302537c884c7dddac92bd23cdbaa90860
gzhawk <gzhawk@hotmail.com>
2018-12-27 10:15:41 +0800

    change some comments in sample code

--- sys_main/UI/UI_font2.h
+++ sys_main/UI/UI_font2.h
@@ -1,5 +1,5 @@
 /* 
-    Demo for teaching how to use 'full' chinese font
+    Demo for teaching how to use 'Extended Font'
     Author: Hawk
 	Email : hawk.gao@ftdichip.com
 	Date  : 2018/Dec
@@ -11,59 +11,59 @@
 #error "program res/font2/eveflh.bin to eve-connected-flash, then comment this line"
 #endif
 
+/* look this file as a input text string file */
 #define TXT_FILE ROOT_PATH"font2\\font2.txt"
-
-#define FONT_NUM 6
-
-#define FONT_W 40
-#define FONT_H 40
-
-#define LINE1_COLOR COLOR_RGB(0,0xFF,0)
-#define LINE2_COLOR COLOR_RGB(0,0,0xFF)
-#define LINE3_COLOR COLOR_RGB(0xFF,0xFF,0xFF)
-
-#define FONT_LEN 10
-#define FONT_LINE 3
-
-#define UTF8_LEN  3//ONE UFT8 char == THREE bytes
-#define LINE1_LEN 15
-#define LINE2_LEN 23
-#define LINE3_LEN 7
-FTU8 line1[LINE1_LEN*UTF8_LEN+1] = {0}; //extra 1 for '\0'
-FTU8 line2[LINE2_LEN*UTF8_LEN+1] = {0}; 
-FTU8 line3[LINE3_LEN*UTF8_LEN+1] = {0}; 
-
-/* this demo's GLYPH file store in address 4096 of the eve-connected-flash
+/* 
+   this demo's GLYPH file store in address 4096 of the eve-connected-flash
    should be the same address you input in EAB tools while generate xfont/glyph
    it's address would be saved in XFONT+0x20 offset
    the value would be "0x800000 | (address / 32)", e.g. "4096" would be "0x800080"
-   
+
    if you put your GLYPH in other address than the prevous address input in EAB
    you may also follow above formula, change the data in XFONT+0x20 
    then EVE would be able to find it 
-   
+
    during generate font by using EAB,
-   some convert set may not bring good display effect for example:
+   some convert set may not bring good display effect, for example:
    simfang.ttf+ASTC10x5+FontSize28 will display abnormal
    while below set would bring good display
    simfnag.ttf+ASTC10x5+FontSize32 will display normal
-   */
+
+   open eveflh.map to see the address of each file in flash
+*/
 #define FONT_XFONT "EVEFLH@9530944:4544" 
 #define FONT_ADDR RAM_G
+#define FONT_NUM 6
+#define FONT_W   40
+#define FONT_H   40
+/*ONE UFT8 char == THREE bytes*/
+#define UTF8_LEN 3
+#define DISP_LEN    (EVE_LCD_WIDTH/FONT_W)
+#define LINE1_COLOR COLOR_RGB(0xFF,   0,   0)
+#define LINE2_COLOR COLOR_RGB(0   ,0xFF,   0)
+#define LINE3_COLOR COLOR_RGB(   0,   0,0xFF)
+/*extra 1 for '\0'*/
+FTU8 line1[DISP_LEN*UTF8_LEN+1] = {0};
+FTU8 line2[DISP_LEN*UTF8_LEN+1] = {0}; 
+FTU8 line3[DISP_LEN*UTF8_LEN+1] = {0}; 
 
 FTVOID font2_main (FTU32 para)
 {
     static FTU8 load = 1;
-    /* instead of using index, coder may directly use 'unicode'
-       in code while using BT81X, to display the customer font
+    /* 
        GLYPH: 
-       1. the customer font 'image' resource
+       1. the customer font 'bitmap' resource
        2. can be stored in eve-connected-flash, or RAM_G of EVE
+       
        XFONT:
-       1. the header of each customer font 'image'
+       1. the Extended Font Metrics Block, for EVE to retrieve font 'bitmap' from GLYPH file
        2. has to be stored in RAM_G of EVE
+       
        both GLYPH and XFONT are necessary when using 'unicode' font
-       */
+       while customer font character liberary increase, 
+       GLYPH file increase, while XFONT file not increase that much
+       so better store GLYPH in eve-connected-flash.
+     */
     if (load) {
         appEVEFLHSetFull();
         if (appEVEFLHToEVE(appEVEFLHAddr(FONT_XFONT), FONT_ADDR, appEVEFLHLen(FONT_XFONT))) {
@@ -73,9 +73,10 @@ FTVOID font2_main (FTU32 para)
         load = 0;
     }
 
-	if (!UTF8_Pump(TXT_FILE,line1,LINE1_LEN) || 
-        !UTF8_Pump(TXT_FILE,line2,LINE2_LEN) ||
-        !UTF8_Pump(TXT_FILE,line3,LINE3_LEN)) {
+    /*keep pumping test data as customer random input for display*/
+	if (!UTF8_Pump(TXT_FILE,line1,DISP_LEN) || 
+        !UTF8_Pump(TXT_FILE,line2,DISP_LEN) ||
+        !UTF8_Pump(TXT_FILE,line3,DISP_LEN)) {
         DBGPRINT;
     };
 
@@ -87,21 +88,12 @@ FTVOID font2_main (FTU32 para)
 	CoCmd_SETFONT2(FONT_NUM, FONT_ADDR, 0);
 
     /* show how to impliment customer font directly on screen */
-    /* 
-       as long as your compiler can recognize UTF-8, you can do whatever you like while coding.
-       for this demo, I below two things to make sure my platform compiler recognize it:
-       1. make this file in UTF-8 format 
-          (simplest way is open your file in binary editer, add "0xEF, 0xBB, 0xEF" at your file head)
-       2. use 'u8' to indicate the compiler for the UFT-8 string
-     */
 	HAL_CmdBufIn(LINE1_COLOR);
 	CoCmd_TEXT(0,0,FONT_NUM,0,line1);
-	
     HAL_CmdBufIn(LINE2_COLOR);
 	CoCmd_TEXT(EVE_LCD_WIDTH/2,EVE_LCD_HIGH/2,FONT_NUM,OPT_CENTERX,line2);
-	
     HAL_CmdBufIn(LINE3_COLOR);
-	CoCmd_TEXT(EVE_LCD_WIDTH-FONT_W*LINE3_LEN,EVE_LCD_HIGH-FONT_H,FONT_NUM,0,line3);
+	CoCmd_TEXT(0,EVE_LCD_HIGH-FONT_H,FONT_NUM,0,line3);
     
     /* after finish UTF-8 text display, in some cases, 
        add below command to avoid some corner cases */
